488b680dcee428b66706f049eb78f19c684eea95
lavalse <lavalselike@gmail.com>
2021-05-27 00:40:17 +0900

    init

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ const server = http.createServer((req,res)=>{
 const port = 3000;
 const ip = "127.0.0.1";
 
-server.listen(port,ip,()=>{
-  console.log(`Server is running at http://${ip}:${port}`);
-});
+server.listen(process.env.PORT);
