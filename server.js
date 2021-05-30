097b120814eb78e511e1714ccca36e1c7c180684
lavalse <lavalselike@gmail.com>
2021-05-30 23:36:36 +0900

    update

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const server = http.createServer((req,res)=>{
               filesList[i] = {name:e};
               filesList[i].size = Math.ceil(parseInt(stats.size)/1024);
               filesList[i].mtime = timeConvert(stats.mtime);
-              filesList[i].path = `http://${process.env.PORT}${url}/${e}`;
+              filesList[i].path = `${url}/${e}`;
               if(stats.isDirectory()){
                 filesList[i].isFolder = true;
               }else{
